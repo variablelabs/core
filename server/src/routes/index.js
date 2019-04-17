035834c08bb0ce6bc5fd193f9c7b5005bf052acb
parthvshah <parthvshah@gmail.com>
2019-04-17 20:59:49 +0530

    Revert "Base for asset"
    This reverts commit e2c1f1afefb91b0ff8c60b9d36489a62f8e58c5f.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,9 +2,6 @@ import Authentication from '../controllers/authentication'
 import Middlewares from './middlewares'
 import api from './api'
 
-import User from '../models/user'
-import Asset from '../models/asset'
-
 const router = require('express').Router()
 
 router.use('/api', Middlewares.loginRequired, api)
@@ -12,22 +9,5 @@ router.post('/signup', Authentication.signup)
 router.post('/signin', Authentication.signin)
 router.get('/ping', (req, res) => res.send('pong'))
 router.get('/', (req, res) => res.json({'source': 'https://github.com/variablelabs/core'}))
-
-// Add '/api' to each route to protect
-router.get('/asset', (req, res) => {
-    return res.status(200).send({
-        success: true,
-    });
-})
-router.post('/asset/create', (req, res) => {
-    return res.status(200).send({
-        success: true,
-    });
-})
-router.post('/asset/modify', (req, res) => {
-    return res.status(200).send({
-        success: true,
-    });
-})
 
 export default router;
